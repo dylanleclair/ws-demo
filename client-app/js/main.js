d6b1fc458f41b6772390d279cd13b551824d6df4
dylanleclair <dylan.leclair@icloud.com>
2022-11-22 14:50:42 -0700

    update

--- client-app/js/main.js
+++ client-app/js/main.js
@@ -1,12 +1,17 @@
-const socket = new WebSocket('ws://localhost:8080')
+// this code runs in the browser!
+const socket = new WebSocket('ws://localhost:8080')     // connect to websocket
 
-console.log("Is this thing on?")
+console.log("Is this thing on?")                        // make sure script is loading properly.
 
-// listen for messages
+// listen for messages from the server!
 socket.onmessage = ({ data }) => {
+    // print out any messages, along with the data sent by the server.
     console.log(`Message from the server: ${data}`);
 }
 
+
+// find the button in the HTML loading this script
 document.querySelector('button').onclick = () => {
+    // on button click, send "Hello world!" to server via websockets!
     socket.send("Hello world!");
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
-const WebSocket = require('ws'); // import websockets
-const server = new WebSocket.Server({ port: '8080' })
+const WebSocket = require('ws');                        // import websockets
 
-server.on('connection', socket => {
-    socket.on('message', message => {
-        socket.send(`Roger that! ${message}`);
+const server = new WebSocket.Server({ port: '8080' })   // start websocket server
+
+server.on('connection', socket => {                     // when a user connects to the websocket
+
+
+    // bind the following 'message' to the websocket
+    socket.on('message', message => {                   // when a client sends data to websocket
+        socket.send(`Roger that! ${message}`);          // reply with a message, echoing their message
     });
 });
+
